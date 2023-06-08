44eae5a6b4594273bc84f404117295d26910e941
Josh <134442015+Blixxky@users.noreply.github.com>
2023-06-08 07:05:56 -0400

    Reverted to old HSBotRunner.bat
    Compatibility issues

--- HSbotRunner.bat
+++ HSbotRunner.bat
@@ -1,13 +1,4 @@
-@echo off
-setlocal
-
-py -3 prereqs_win.py
-
-py -3.11 -m venv "MFB"
-if errorlevel 1 (
-    echo Python 3.11 not found, creating virtual environment with Python 3...
-    py -3 -m venv MFB
-)
-MFB\Scripts\python.exe main.py
-endlocal
+py -3.11 -m venv %~dp0MFB
+%~dp0MFB\Scripts\python.exe -m pip install -r requirements_win.txt
+%~dp0MFB\Scripts\python.exe main.py
 pause
